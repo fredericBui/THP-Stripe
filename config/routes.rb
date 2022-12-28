348e1d14b25c987e1e8abda888d3abd420827869
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-28 11:54:36 +0100

    routing to home and checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/checkout', to: 'checkout#index'
+  
+  scope '/checkout' do
+    get '/', to: 'checkout#index'
+    get '/success', to: 'checkout#success'
+    get '/cancel', to: 'checkout#cancel'
+  end
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
 end
