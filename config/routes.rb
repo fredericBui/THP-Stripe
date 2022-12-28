0ce1ba3e111f05a760277e1471ed307929e7198b
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-28 12:37:30 +0100

    thpcheckout routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
     get '/cancel', to: 'checkout#cancel'
   end
   
+  scope '/thpcheckout' do
+    post 'create', to: 'thpcheckout#create', as: 'thpcheckout_create'
+    get 'success', to: 'thpcheckout#success', as: 'thpcheckout_success'
+    get 'cancel', to: 'thpcheckout#cancel', as: 'thpcheckout_cancel'
+  end
+
   # Defines the root path route ("/")
   root 'home#index'
 end
